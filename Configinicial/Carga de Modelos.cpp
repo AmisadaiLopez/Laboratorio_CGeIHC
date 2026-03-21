81b91e79274c409ee3455eb5849ab7a23d87395b
AmisadaiLopez <147724801+AmisadaiLopez@users.noreply.github.com>
2026-03-20 20:18:41 -0600

    Adjunto los archivos agregados de la práctica 6, donde agrego todos los modelos que utilice para esta pŕactica

--- Configinicial/Carga de Modelos.cpp
+++ Configinicial/Carga de Modelos.cpp
@@ -1,7 +1,7 @@
 //López Hernández Miriam Amisadai
 //320260366
-//Fecha de entrega: 12/03/2026
-//Previo 6
+//Fecha de entrega: 20/03/2026
+//Practica 6
 
 
 // Std. Includes
@@ -60,7 +60,7 @@ int main( )
     glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Carga de modelos y camara sintetica - Miriam Lopez - Previo 6", nullptr, nullptr );
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Carga de modelos y camara sintetica - Miriam Lopez - Practica 6", nullptr, nullptr );
     
     if ( nullptr == window )
     {
@@ -101,7 +101,15 @@ int main( )
     
     // Load models
     Model dog((char*)"Models/RedDog.obj");
+	Model grass((char*)"Models/grass.obj");
+    Model house((char*)"Models/House.obj");
+	Model doghouse((char*)"Models/doghouse.obj");
+	Model tree((char*)"Models/Tree.obj");
+	Model arbusto((char*)"Models/arbusto.obj");
+	Model plato((char*)"Models/Dog Food and Water Bowl.obj");
+
     glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
+    
     
   
 
@@ -118,7 +126,7 @@ int main( )
         DoMovement();
 
         // Clear the colorbuffer
-        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClearColor(0.53f, 0.81f, 0.98f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.Use();
@@ -127,16 +135,70 @@ int main( )
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-        // Draw the loaded model
+        // Dibujar el perro en el centro
         glm::mat4 model(1);
+        model = glm::translate(model, glm::vec3(0.0f, -0.15f, 1.0f));
+        model = glm::scale(model, glm::vec3(1.15f, 1.15f, 1.15f));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-		dog.Draw(shader);
+        dog.Draw(shader);
 
-		model = glm::translate(model, glm::vec3(3.0f, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-		dog.Draw(shader);
+        //Dibujar el pasto
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-5.0f, -1.45f, -10.0f));
+        model = glm::scale(model, glm::vec3(7.0f, 7.0f, 7.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        grass.Draw(shader);
 
+        // Dibujar la casa 
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(0.0f, -0.6f, -0.75f));
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        house.Draw(shader);
+
+		//dibujar la casa del perro desplazada a la izquierda
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(-1.8f, -0.8f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.8f, 0.8f, 0.8f));
+		model = glm::rotate(model, glm::radians(25.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1,GL_FALSE, glm::value_ptr(model));
+		doghouse.Draw(shader);
+
+		//dibujar el plato del perro desplazado a la izquierda
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(-1.0f, -0.6f, 2.0f));
+		model = glm::scale(model, glm::vec3(0.015f, 0.015f, 0.015f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1,GL_FALSE, glm::value_ptr(model));
+		plato.Draw(shader);
+        
+		//dibujar el arbol desplazado a la derecha
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(5.0f, -0.6f, 1.0f));
+		model = glm::scale(model, glm::vec3(0.5f, 1.5f, 0.5f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1,GL_FALSE, glm::value_ptr(model));
+		tree.Draw(shader);
+
+        //dibujar el arbol desplazado a la izquierda
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-0.5f, -0.7f, -0.5f));
+        model = glm::scale(model, glm::vec3(0.5f, 1.5f, 0.5f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        tree.Draw(shader);
+
+		//dibujar el arbusto desplazado a la derecha
+		model = glm::mat4(1);
+		model = glm::translate(model, glm::vec3(5.0f, -0.6f, 1.2f));
+		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1,GL_FALSE, glm::value_ptr(model));
+		arbusto.Draw(shader);
+
+        //dibujar el arbusto desplazado a la izquierda
+        model = glm::mat4(1);
+        model = glm::translate(model, glm::vec3(-4.0f, -0.6f, 1.5f));
+        model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        arbusto.Draw(shader);
 
         
 
